a84ce1c2b30545d23fe78f06cfe6ad8782a51e5c
guardina <guardinialex@gmail.com>
2024-03-22 15:24:05 +0100

    Now prints information extracted in correct format into html file

--- excel.php
+++ excel.php
@@ -30,12 +30,54 @@ function nextColumn($column) {
 }
 
 
+function isExcelDate($value) {
+    return is_numeric($value) && $value > 40000 && $value < 50000 && floor($value) == $value;
+}
 
 
+function excelDateToUnixTimestamp($excelDate) {
+    $unixEpoch = strtotime('1899-12-30 00:00:00'); 
+    
+    if ($excelDate >= 60) {
+        $excelDate++;
+    }
+
+    $unixTimestamp = ($excelDate - 1) * 86400 + $unixEpoch;
+    return date('Y-m-d', $unixTimestamp);
+}
+
+
+
+
+function writeOnHTML($sheet, $filename, $currentColumn, $currentRow){
+    if ($sheet->getCell($currentColumn . $currentRow)->getDataType() === \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_FORMULA) {
+        $cellValue = $sheet->getCell($currentColumn . $currentRow)->getCalculatedValue();
+    } else if (isExcelDate($sheet->getCell($currentColumn . $currentRow)->getValue())) {
+        $cellValue = excelDateToUnixTimestamp($sheet->getCell($currentColumn . $currentRow)->getValue());
+    } else {
+        $cellValue = $sheet->getCell($currentColumn . $currentRow)->getValue();
+    }
+    
+
+    $cellColor = $sheet->getStyle($currentColumn . $currentRow)->getFill()->getStartColor()->getRGB();
+
+    if ($cellValue == null || $cellValue == "") {
+        return "<td></td>"; 
+    }
+
+
+    if ($filename == 'neu VB in Arbeit Kursanmeldungen 2023_2024.xlsx' && $currentRow >= 198 && $currentRow <= 206) {
+        return "<td style='color: $cellColor;'>$cellValue</td>";
+    } else {
+        return "<td>$cellValue</td>";
+    }
+}
 
 
 
 $directory = "excels/";
+
+$html = "<html><head><title>Extracted information</title></head><body>";
 
 foreach (scandir($directory) as $filename) {
     if (pathinfo($filename, PATHINFO_EXTENSION) == 'xlsx' || pathinfo($filename, PATHINFO_EXTENSION) == 'xls') {
@@ -45,29 +87,78 @@ foreach (scandir($directory) as $filename) {
         $sheet = $data->getActiveSheet();
         $highestRow = $sheet->getHighestRow();
         $highestColumn = $sheet->getHighestColumn();
-        echo "$highestColumn\n";
         $column_names = [];
 
         $currentColumn = 'A';
 
         if ($filename == '2024 Restmittel.xlsx') {
-            $row = 14;
+            $titleRow = 14;
         } else if ($filename == 'neu VB in Arbeit Kursanmeldungen 2023_2024.xlsx') {
-            $row = 4;
+            $titleRow = 4;
         } else {
-            $row = 1;
+            $titleRow = 1;
         }
 
+        $html .= "<table>";
+        $html .= "<p>$filename</p>";
+
+        $html .= "<tr>";
+
+        $html .= "<th>ID</th>";
+
         while ($currentColumn != $highestColumn) {
-            $cellValue = $sheet->getCell($currentColumn . $row)->getValue();
-            //echo "Processing column: $cellValue\n"; // Debugging output
-            $column_names[] = $sheet->getCell($currentColumn . $row)->getValue();
+            $titleCellValue = $sheet->getCell($currentColumn . $titleRow)->getValue();
+            $html .= "<th>$titleCellValue</th>";    
+            $column_names[] = $titleCellValue;
+
             $currentColumn = nextColumn($currentColumn);
         }
 
-        echo "File: $filename\n";
-        echo "Column names: " . implode(", ", $column_names) . "\n";
+        $html .= "</tr>";
+
+        $currentRow = $titleRow+1;
+
+
+        for ($row = $highestRow; $row >= 1; $row--) {
+            $cellValue = $sheet->getCell('A' . $row)->getValue();
+            if (!empty($cellValue)) {
+                $lastRow = $row;
+                break;
+            }
+        }
+
+        
+        while ($currentRow <= $lastRow) {
+            $html .= "<tr>";
+            $html .= "<td>$currentRow</td>";
+            $currentColumn = 'A';
+            $empty = true;
+            while ($currentColumn != $highestColumn) {
+                $html .= writeOnHTML($sheet, $filename, $currentColumn, $currentRow);
+                
+                $currentColumn = nextColumn($currentColumn);
+            }
+
+            $html .= writeOnHTML($sheet, $filename, $currentColumn, $currentRow);
+            $currentRow++;
+
+            if (!$empty) {
+                echo "\n";
+            }
+            $html .= "</tr>";
+        }
     }
+
+    $html .= "</table>";
+    $html .= "<br>";
+    $html .= "<br>";
+    $html .= "<br>";
+    $html .= "<br>";
+    
 }
+
+$html .= "</body></html>";
+
+file_put_contents('extracted_information.html', $html);
 
 ?>
